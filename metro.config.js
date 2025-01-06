cb27609b3c8bddac4a89d39437f74d009521fd8a
aura24x7 <gamingaura24x7@gmail.com>
2025-01-06 18:44:46 +0530

    Fix Firebase auth module resolution and update dependencies

--- metro.config.js
+++ metro.config.js
@@ -7,11 +7,4 @@ defaultConfig.resolver.sourceExts.push('cjs');
 
 defaultConfig.resolver.assetExts.push('cjs');
 
-defaultConfig.resolver.extraNodeModules = {
-  ...defaultConfig.resolver.extraNodeModules,
-  '@firebase/app': require.resolve('@firebase/app'),
-  '@firebase/auth': require.resolve('@firebase/auth'),
-  '@firebase/firestore': require.resolve('@firebase/firestore'),
-};
-
 module.exports = defaultConfig;
